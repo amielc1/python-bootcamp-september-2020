1968c88a3e3a3cae89b1f8987f73b8fdb8794d93
Amiel Cohen <amielc1@gmail.com>
2020-09-06 21:59:25 +0300

    Ex1 : the_bigger_number

---
+++ Exercises/LoopsAndConditions/TheBiggerNum.py
@@ -0,0 +1,10 @@
+def the_bigger_number(list_of_numbers):
+    big = -1
+    for num in list_of_numbers:
+        try:
+            num = float(num)
+            if num > big:
+                big = num
+        except ValueError:
+            raise ValueError(f"{num} is not a number")
+    return  big
---
+++ Exercises/tests/the_bigger_number_test.py
@@ -0,0 +1,19 @@
+import unittest
+from TheBiggerNum import the_bigger_number
+
+not_numbers_list = [3, "3", "3r4", 6, 5, 34, 322.3, 22, 100]
+int_numbers_list = [3, 3, 34, 6, 5, 34, 322.3, 22, 100, 0]
+str_numbers_list = [3, "3", "34", 6, 5, 34, 322.3, "22", 100]
+
+class the_bigger_number_test(unittest.TestCase):
+
+    def test_int_list_pass(self):
+        my_big_num = the_bigger_number(int_numbers_list)
+        py_big_num = max(int_numbers_list)
+        self.assertEqual(my_big_num, py_big_num)
+
+    def test_not_numbers_list_raise_exception(self):
+        self.assertRaises(ValueError,the_bigger_number,not_numbers_list)
+
+    def test_str_numbers_list_pass(self):
+        self.assertEqual(the_bigger_number(str_numbers_list),322.3)
