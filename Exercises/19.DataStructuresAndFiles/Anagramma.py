5fdb61c8bcd42804e4dce9aaedc766bb5e713f99
Amiel Cohen <amielc1@gmail.com>
2020-09-09 21:24:53 +0300

    anagrams

--- Exercises/19.DataStructuresAndFiles/Anagramma.py
+++ Exercises/19.DataStructuresAndFiles/Anagramma.py
@@ -1,28 +1,26 @@
 import collections
 
 
-def is_anagramma(word1, word2):
+def is_anagramma(word1, word2) -> bool:
     counter1 = collections.Counter(list(word1))
     counter2 = collections.Counter(list(word2))
-    print(counter1)
-    print(counter2)
-    if counter1.__eq__(counter2):
-        print("counters equals")
-    else:
-        print("counters not equals")
+    return counter1 == counter2
 
-is_anagramma("aas", "saa")
-list = ["aas", "saa", "ffo""off"]
 
-list = ["1", "2", 4.0, "3"]
-print(max(map(float, list)))
+print(is_anagramma("aas", "aas"))
+print(is_anagramma("das", "saa"))
+anagramas = ["dad", "add", "lab", "aas", "bal"]
+# anagramas_dic = {}
+# for word in anagramas:
+#     anagramas_dic[word]=collections.Counter(word)
+# for i in range(anagramas_dic):
+#     if
 
-#     1 add
-#     2 dad
-#     3 help
-#     4 more
-#     5 rome
-#
-# 12 13 14 15
-# 21 23 24 25
-# 31 32 24 35
+# for i in range(anagramas)-1:
+#     if is_anagramma(anagramas[i],anagramas[i+1]):
+
+# dad
+# add
+# lab
+# aas
+# bal
