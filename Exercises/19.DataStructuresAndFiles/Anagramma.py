89b84f190a836788d7a30f99536a9a53965d8865
Amiel Cohen <amielc1@gmail.com>
2020-09-12 23:03:27 +0300

    anagrams

--- Exercises/19.DataStructuresAndFiles/Anagramma.py
+++ Exercises/19.DataStructuresAndFiles/Anagramma.py
@@ -7,11 +7,22 @@ def is_anagramma(word1, word2) -> bool:
     return counter1 == counter2
 
 
-print(is_anagramma("aas", "aas"))
-print(is_anagramma("das", "saa"))
-anagramas = ["dad", "add", "lab", "aas", "bal"]
+anagrams = ["dad", "add", "lab", "aas", "bal", "dda"]
+anagrams_dics = {word: collections.Counter(list(word)) for word in anagrams}
 
-index = 0
-for i in range(index, 5):
-    for j in range(index + 1, 5):
-        print(f"({i},{j})")
+dic_found = {}
+
+
+def foo(ang_dict):
+    index = 0
+    for dic1_k, dic1_v in ang_dict.items():
+        print(f"----> Cycle {index}")
+        for dic2_k, dic2_v in ang_dict.items():
+            if dic1_v == dic2_v:
+                print(f"FOUND $${dic1_k},{dic2_k}$$")
+            else:
+                print(f"{dic1_k},{dic2_k}")
+        index += 1
+
+
+foo(anagrams_dics)
