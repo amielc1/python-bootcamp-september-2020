4fd2ce6b90945aed0a29b8dd89894064fda5608b
Amiel Cohen <amielc1@gmail.com>
2020-09-13 09:55:54 +0300

    lesson 3

---
+++ Exercises/21.Class/3..py
@@ -0,0 +1,24 @@
+class Widget:
+    pass
+
+
+luke    = Widget("Luke")
+hansolo = Widget("Han Solo")
+leia    = Widget("Leia")
+yoda    = Widget("Yoda")
+padme   = Widget("Padme Amidala")
+anakin  = Widget("Anakin Skywalker")
+obi     = Widget("Obi-Wan")
+darth   = Widget("Darth Vader")
+_all    = Widget("")
+
+
+luke.add_dependency(hansolo, leia, yoda)
+leia.add_dependency(padme, anakin)
+obi.add_dependency(yoda)
+darth.add_dependency(anakin)
+
+_all.add_dependency(luke, hansolo, leia, yoda, padme, anakin, obi, darth)
+_all.build()
+# code should print: Han Solo, Padme Amidala, Anakin Skywalker, Leia, Yoda, Luke, Obi-Wan, Darth Vader
+# (can print with newlines in between modules)
