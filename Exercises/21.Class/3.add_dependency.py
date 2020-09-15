82c999784ef86b9e60e2b8a2cef4c828b2270234
Amiel Cohen <amielc1@gmail.com>
2020-09-15 16:23:08 +0300

    Ex add_dependency

---
+++ Exercises/21.Class/3.add_dependency.py
@@ -0,0 +1,39 @@
+class Widget:
+
+    def __init__(self, name):
+        self.name = name
+        self.is_built = False
+        self.dependency = []
+
+    def add_dependency(self, *widgetes):
+        for widget in widgetes:
+            print(f"{self.name} depend on {widget.name}")
+            self.dependency.append(widget)
+
+    def build(self):
+        self.is_built = True
+        for widget in self.dependency:
+            if not widget.is_built:
+                widget.build()
+                print(f"Build {widget.name} for build {self.name}")
+
+
+luke = Widget("Luke")
+hansolo = Widget("Han Solo")
+leia = Widget("Leia")
+yoda = Widget("Yoda")
+padme = Widget("Padme Amidala")
+anakin = Widget("Anakin Skywalker")
+obi = Widget("Obi-Wan")
+darth = Widget("Darth Vader")
+_all = Widget("Widget all")
+
+luke.add_dependency(hansolo, leia, yoda)
+leia.add_dependency(padme, anakin)
+obi.add_dependency(yoda)
+darth.add_dependency(anakin)
+
+_all.add_dependency(luke, hansolo, leia, yoda, padme, anakin, obi, darth)
+_all.build()
+# code should print: Han Solo, Padme Amidala, Anakin Skywalker, Leia, Yoda, Luke, Obi-Wan, Darth Vader
+# (can print with newlines in between modules)
